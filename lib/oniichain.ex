6f2bfe925b81119da3c7df89f15ae3943430cd5c
Arvin Sharma <Arvin@Arvins-MacBook-Pro.local>
2017-12-09 11:20:33 -0800

    project setup

---
+++ lib/oniichain.ex
@@ -0,0 +1,18 @@
+defmodule Oniichain do
+  @moduledoc """
+  Documentation for Oniichain.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Oniichain.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oniichain_test.exs
@@ -0,0 +1,8 @@
+defmodule OniichainTest do
+  use ExUnit.Case
+  doctest Oniichain
+
+  test "greets the world" do
+    assert Oniichain.hello() == :world
+  end
+end
