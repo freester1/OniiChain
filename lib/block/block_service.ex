df5b4e69e9e24fb01aadd4f2753494b9f8b36feb
Arvin Sharma <freester@baited.me>
2017-12-09 12:37:10 -0800

    basics

--- lib/block/block_service.ex
+++ lib/block/block_service.ex
@@ -1,15 +1,41 @@
 defmodule Oniichain.BlockService do
+    require Logger
     @moduledoc """
     Operations for blocks
     """
 
-    # adds a new block with the given data to the chain
-    def add_block(data) do
-        
+    # creates a new block based on the latest one
+    def create_next_block(data) do
+        latest_block = :ets.lookup(:block_chain, :latest) |> hd |> elem(1)
+        index        = latest_block.index + 1
+        timestamp    = :os.system_time(:seconds)
+        hash         = generate_block_hash(index, latest_block.hash, timestamp, data)
+        %Oniichain.Block{
+            index: index,
+            previous_hash: latest_block.hash,
+            timestamp: timestamp,
+            data: data,
+            hash: hash
+        }
+    end
+
+    def validate_block(new_block, previous_block) do
+        if new_block.index - 1 != previous_block.index 
+        || new_block.previous_hash != previous_block.hash 
+        || generate_hash_from_block(new_block) != new_block.hash do
+            Logger.debug("Invalid block new_block: #{new_block} prev_block: #{previous_block}")
+            false
+        end
+        true
+    end
+
+    defp generate_hash_from_block(block) do
+        generate_block_hash(block.index, block.previous_hash, block.timestamp, block.data)
     end
 
     # generates a block hash
     defp generate_block_hash(index, previous_hash, timestamp, data) do
-        :crypto.hash()
+        :crypto.hash(:sha256, "#{index}#{previous_hash}#{timestamp}#{data}")
+        |> Base.encode64
     end
 end
--- lib/oniichain/application.ex
+++ lib/oniichain/application.ex
@@ -12,11 +12,28 @@ defmodule Oniichain.Application do
       # {Test.Worker, arg},
     ]
 
-    # setup in memory store for block chain
+    initialize_datastore()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Test.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp initialize_datastore() do
+    :ets.new(:block_chain, [:set, :public, :named_table])
+    generate_initial_block()
+  end
+
+  defp generate_initial_block() do
+    init_block = %Oniichain.Block{
+      index: 0,
+      previous_hash: "0", 
+      timestamp: :os.system_time(:seconds), 
+      data: "foofizzbazz", 
+      hash: :crypto.hash(:sha256, "cool") |> Base.encode64
+    }
+    :ets.insert(:block_chain, {0, init_block})
+    :ets.insert(:block_chain, {:latest, init_block})
   end
 end
